b6ab90cab8e0cb0f4375c44c58a777cfcf46a001
Swasti Mishra <91975777+Swasti222001@users.noreply.github.com>
2022-12-23 19:57:34 +0530

    Layout made

---
+++ my-app/src/layout/Mainlayout.js
@@ -0,0 +1,11 @@
+// call your components here
+//style them in mainlayout.css
+import React from 'react'
+
+const Mainlayout = () => {
+  return (
+    <div>Mainlayout</div>
+  )
+}
+
+export default Mainlayout
